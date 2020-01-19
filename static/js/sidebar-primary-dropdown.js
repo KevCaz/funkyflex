f7e83f41866c6bed214f4df1acf5acf28ad93f72
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-01-19 09:31:12 -0500

    improve dropdown primary sidebar

--- static/js/sidebar-primary-dropdown.js
+++ static/js/sidebar-primary-dropdown.js
@@ -1,9 +1,8 @@
 // https://www.w3schools.com/howto/howto_js_dropdown.asp
-function dropDown() {
-    var x = document.getElementById("dropdown")
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-      x.style.flexFlow = "row wrap";
+function dropDown(x) {
+    var x = document.getElementById("dropdown-" + x)
+    if (x.style.display == "none") {
+      x.style.display = "block";
     } else {
       x.style.display = "none";
     }
