fd40a7d3e73d1bc8679080210ab0143987708ce8
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-01-18 19:08:44 -0500

    format list and single

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 [![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 
 
-This theme is designed to be minimal. It included:
+This theme is designed to be minimal. It includes:
 
 - https://fontawesome.com/
 - https://jpswalsh.github.io/academicons/
--- static/js/sidebar-secondary.js
+++ static/js/sidebar-secondary.js
@@ -1,13 +1,14 @@
-var x = document.querySelectorAll(".ctt-main-content h1, .ctt-main-content h2");
+var x = document.querySelectorAll(".ctt-main-content .article-main h1, .ctt-main-content .article-main h2");
 var i;
 
-
 for (i = 0; i < x.length; i++) {
-  id = "sidebar_secondary_id_" + i;
-  x[i].id = id;
-  if (x[i].tagName == "H1") {
-    document.write("<h1> <a href='#" + id + "'>"+ x[i].innerHTML + "</a></h1>");
-  } else if (x[i].tagName == "H2") {
-    document.write("<h2> <a href='#" + id + "'>"+ x[i].innerHTML + "</a></h2>");
+  if (x[i].id != "title") {
+    id = "sidebar_secondary_id_" + i;
+    x[i].id = id;
+    if (x[i].tagName == "H1") {
+      document.write("<h1> <a href='#" + id + "'>" + x[i].innerHTML + "</a></h1>");
+    } else if (x[i].tagName == "H2") {
+      document.write("<h2> <a href='#" + id + "'>" + x[i].innerHTML + "</a></h2>");
+    }
   }
 }
