0a8c30fdb06c009ed98f57f85cf943ab6f5b96ec
Kevin Cazelles <kcazelle@uoguelph.ca>
2020-01-19 19:29:01 -0500

    improve primary sidebar responsiveness

--- static/js/sidebar-primary-dropdown.js
+++ static/js/sidebar-primary-dropdown.js
@@ -1,9 +1,13 @@
 // https://www.w3schools.com/howto/howto_js_dropdown.asp
 function dropDown(x) {
-    var x = document.getElementById("dropdown-" + x)
+    var x = document.getElementById(x)
     if (x.style.display == "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
   }
+
+function changeAfter(x) {
+  var x = document.getElementById(x)
+}
--- static/js/sidebar-secondary.js
+++ static/js/sidebar-secondary.js
@@ -8,7 +8,7 @@ for (i = 0; i < x.length; i++) {
     if (x[i].tagName == "H1") {
       document.write("<h1> <a href='#" + id + "'>" + x[i].innerHTML + "</a></h1>");
     } else if (x[i].tagName == "H2") {
-      document.write("<h2> <a href='#" + id + "'>" + x[i].innerHTML + "</a></h2>");
+      document.write("<h2> <a href='#" + id + "'>" +  x[i].innerHTML + "</a></h2>");
     }
   }
 }
